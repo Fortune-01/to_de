71a66d63916c7d15aeb0e6e78232c53af9116f3c
fortune-0 <fortunepeterspc07@gmail.com>
2022-10-17 07:50:25 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,20 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <stdio.h>
+/**
+ * main -Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char acharacter;
+	int ainteger;
+	float afloat;
+	long along;
+	long long alonglong;
+
+	printf("size of a char: %lu byte(s)\n", sizeof(acharacter));
+	printf("size of an int: %lu byte(S)\n", sizeof(ainteger));
+	printf("size of a float: %lu byte(s)\n", sizeof(afloat));
+	printf("size of a long int: %lu byte(s)\n", sizeof(along));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(alonglong));
+	return (0);
+}
