94494df4f79d3a573aa2a3b64f54ab0ce0888724
fortune-0 <fortunepeterspc07@gmail.com>
2022-10-17 14:46:22 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print all single digits
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	printf("%i", digit);
+	putchar('\n');
+	return (0);
+}
+
