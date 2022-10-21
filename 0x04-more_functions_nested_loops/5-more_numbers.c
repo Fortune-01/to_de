9daa0164289a0166391b9e632540410820bf1b44
fortune-0 <fortunepeterspc07@gmail.com>
2022-10-20 20:03:53 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - prints 0 - 14 ten times
+ * Return: Always 0
+ */
+void more_numbers(void)
+{
+	int num, a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (num = 0; numb <= 14; num++)
+		{
+			if (mum > 9)
+			{
+				_putchar((num / 10) + '0');
+			}
+			_putchar((num / 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
