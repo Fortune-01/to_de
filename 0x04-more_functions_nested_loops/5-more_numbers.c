61e9051addf7ea50c446d8bd091a2831cf699020
fortune-0 <fortunepeterspc07@gmail.com>
2022-10-21 09:02:40 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,11 +9,11 @@ void more_numbers(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (num = 0; numb <= 14; num++)
+		for (num = 0; num <= 14; num++)
 		{
 			if (mum > 9)
 			{
-				_putchar((num / 10) * '0');
+				_putchar((num / 10) + '0');
 			}
 			_putchar((num % 10) + '0');
 		}
