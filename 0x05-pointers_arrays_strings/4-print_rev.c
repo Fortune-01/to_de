e82d80504957643e4cd395dda943df1151706ac3
fortune-0 <fortunepeterspc07@gmail.com>
2022-10-26 06:09:36 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,19 +7,16 @@
  * Return: string in reverse
  */
 void print_rev(char *s)
-
 {
-	char rv = s[0];
-	int c = 0;
-	int i;
+	int b = 0;
 
-	while (s[c] != '\0')
-		c++;
-	for (i = 0; i < c; i++)
+	while (s[b] != '\0')
 	{
-		c--;
-		rv = s[i];
-		s[i] = s[c];
-		s[c] = rv;
+		b++;
 	}
+	for (b -= 1; b >= 0; b++)
+	{
+		_putchar(s[b]);
+	}
+	_putchar('\n');
 }
