2e22d7bbbfb2fdf7840e5711dc81063f06123c7d
fortune-0 <fortunepeterspc07@gmail.com>
2022-10-26 06:25:34 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _put - print string followed by a new line
+ * @str: the string to print
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+	_putchar('\n');
+}
