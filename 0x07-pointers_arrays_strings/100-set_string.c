05d8dc61db92917e8db3a72bb569fc6d9b6e8a8a
fortune-0 <fortunepeterspc07@gmail.com>
2022-11-06 14:00:49 -0800

    voltron

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - function that sets the value of a ponter to a character
+ * @s: pointer to set
+ * @to: pointer to replace s
+ * Return: 0
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
