1af10ef217e30b1b6aafea98009e834a7b47627c
fortune-0 <fortunepeterspc07@gmail.com>
2022-11-17 08:55:55 -0800

    1st try

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers_header.h"
+
+/**
+ * main - funstion that prints a name
+ * @name: name of person, object
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	printf("Prints a name\n");
+	return (0);
+}
