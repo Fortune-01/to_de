bb20681525b326efd21f9f58cea945292323e2b6
fortune-0 <fortunepeterspc07@gmail.com>
2022-11-21 07:02:17 -0800

    update

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * create an array of chars,
+ * and intilizes it wwith a specific char
+ * @size: the size of memory to print
+ *
+ * Return: 0.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+
+	unsigned int i;
+
+	i = 0;
+
+	str = malloc(size);
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (; i < size; i++)
+		/** i+=1
+		 * i=i+1
+		 */
+	{
+		str[i] = c;
+	}
+	return (str);
+}
