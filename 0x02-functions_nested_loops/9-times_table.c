d85e16b98dc8c146cd95ee15558bfa6b1950e166
fortune-0 <fortunepeterspc07@gmail.com>
2022-12-26 03:48:48 -0800

    updatte

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the mulitiplicaton table
+ * mulitples of 9
+ * Returns 0.
+ */
+void times_table(void)
+{
+	int n = 9;
+
+	for (int i = 0; i < 30; i++)
+	{
+		printf("%d X %d = %d\n", n, i, n * i);
+	}
+
+	return;
+}
