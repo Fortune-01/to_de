48bc3d963bf5361ca706d7c6e15c718132c13309
fortune-0 <fortunepeterspc07@gmail.com>
2022-12-27 00:00:16 -0800

    update

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - checks the code
+ * print natural numbers
+ * from zero to 98
+ * using for loop
+ * Return 0.
+ */
+void print_to_98(int n)
+{
+
+	for (n = 0; n <= 98;)
+		printf("the value of n is %d\n", n++);
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
