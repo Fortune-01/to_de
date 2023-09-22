94e1c630004292c566a7ee515c6d7f6d8fdc7145
fortune-01 <fortunepeterspc@gmail.com>
2023-09-22 15:45:28 -0700

    uo

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,23 +1,6 @@
 #include <stdio.h>
 
-/**
- * Function to print the message
- */
-void Message(void)
-{
-    printf("You're beat! and yet, you must allow,\n");
-    printf("I bore my house upon my back!\n");
-}
-int main(void)
-{
-    /** 
-     * Call the printMessage function before the main function logic
-     */
-    Message();
-    
-    printf("(A tortoise, having pretty good sense of");
-    printf(" a hare's nature, challenges one to a race.)\n");
-    
-    return (0);
-}
+void message(void)
 
+{
+	printf(
