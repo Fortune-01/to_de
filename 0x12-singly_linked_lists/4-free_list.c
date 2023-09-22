83174f88ceade5c4da4c7aefc2755c7e0cc9f181
fortune-01 <fortunepeterspc@gmail.com>
2023-09-22 13:11:01 -0700

    free

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -3,15 +3,14 @@
 /** frees a linked list
  */
 
-void free_list(list_t *head);
+void free_list(list_t *head)
 {
-	list_t *tmp;
-	while (head)
-	{
-		tmp = head->next;
-		free(head->str);
-		free(head);
-
-		head = tmp;
-	}
+list_t *tmp;
+while (head)
+{
+tmp = head->next;
+free(head->str);
+free(head);
+head = tmp;
+}
 }
