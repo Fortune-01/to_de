5811417a525f984b0656775b34e06dd0e8fd73e6
fortune-01 <fortunepeterspc@gmail.com>
2023-09-22 13:26:37 -0700

    faster than a hare

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// Function to print the message
+void printMessage() {
+    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+
+int main() {
+    // Call the printMessage function before the main function logic
+    printMessage();
+    
+    // Rest of the main function logic goes here
+    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+    
+    return 0;
+}
+
