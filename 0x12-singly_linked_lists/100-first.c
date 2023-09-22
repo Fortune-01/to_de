df104f634ad217c4fc36ae93ecc3aa77d1c76552
fortune-01 <fortunepeterspc@gmail.com>
2023-09-22 13:44:47 -0700

    faster than a hare

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
-// Function to print the message
-void printMessage() {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+/**
+ * Function to print the message
+ */
+void printMessage(void)
+{
+    printf("You're beat! and yet, you must allow,\n");
+    printf("I bore my house upon my back!\n");
 }
-
-int main() {
-    // Call the printMessage function before the main function logic
+int main(void)
+{
+    /** 
+     * Call the printMessage function before the main function logic
+     */
     printMessage();
     
     // Rest of the main function logic goes here
