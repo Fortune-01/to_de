44772f9c62577909627735f96b5d2bbe0c9e53fd
fortune-01 <fortunepeterspc@gmail.com>
2023-09-23 12:47:01 -0700

    assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,28 +1,9 @@
-section .data
-    hello db 'Hello, Holberton',0xA  ; The string to print, followed by a newline character (0xA)
-    hello_len equ $ - hello           ; Calculate the length of the string
-
-section .text
-    global _start
-
-_start:
-    ; File descriptor for STDOUT (1)
-    mov rdi, 1
-    
-    ; Address of the string to print
-    mov rsi, hello
-    
-    ; Length of the string
-    mov rdx, hello_len
-    
-    ; System call number for write (1)
-    mov rax, 1
-    
-    ; Invoke the syscall
-    syscall
-
-    ; Exit the program
-    mov rax, 60       ; System call number for exit
-    xor rdi, rdi      ; Exit status (0)
-    syscall
-
+ 		global	main
+		extern	printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
